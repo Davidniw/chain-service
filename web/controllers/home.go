ed7795616d9fdc84b938c2ec2d3ed9726b187e96
Alain Jacques <alain.jacques@rockend.com.au>
2017-12-15 10:54:05 +1100

    Attempt struct data loop

--- web/controllers/home.go
+++ web/controllers/home.go
@@ -4,19 +4,20 @@ import (
 	"net/http"
 	"fmt"
 )
-
+var dataVal []string
 func (app *Application) HomeHandler(w http.ResponseWriter, r *http.Request) {
 	helloValue, err := app.Fabric.QueryHello()
+	dataVal = append(dataVal, helloValue)
 	var historyValue = GetRequestHistory()
 	if err != nil {
 		http.Error(w, "Unable to query the blockchain", 500)
 	}
 
 	data := &struct {
-		Hello string
+		Hello []string
 		History []string
 	}{
-		Hello: helloValue,
+		Hello: dataVal,
 		History: historyValue,
 	}
 	fmt.Println("Request: ", r,"Data: ", data)
