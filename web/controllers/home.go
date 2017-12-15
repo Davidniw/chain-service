0ded60069f4c11ae569f1232906dfc9be2da38c3
Alain Jacques <alain.jacques@rockend.com.au>
2017-12-15 13:38:50 +1100

    fix duplicate

--- web/controllers/home.go
+++ web/controllers/home.go
@@ -4,11 +4,7 @@ import (
 	"net/http"
 	"fmt"
 )
-type Hell struct {
-		Guid string
-		Value string
-	}
-var dataStruct []Hell
+
 func (app *Application) HomeHandler(w http.ResponseWriter, r *http.Request) {
 	helloValue, err := app.Fabric.QueryHello()
 	var dataVal = GetRequestHistoryValues()
